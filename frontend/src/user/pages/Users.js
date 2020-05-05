e59e60937f610ad43dff6b64eca5eb7a36aa437d
Wesley Osborne <wao218@lehigh.edu>
2020-05-05 19:49:16 -0400

    Added UserList and UserItem components and styling

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
+  const USERS = [
+    {
+    id: 'u1',
+    name: 'Max Schwarz',
+    image: 'https://source.unsplash.com/user/erondu/',
+    places: '3'
+  }
+];
+
   return (
-    <h2>
-      Users Works!
-    </h2>
-  )
+    <UsersList items={USERS} />
+  );
 }
 
 export default Users;
