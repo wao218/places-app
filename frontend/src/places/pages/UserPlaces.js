29a951f273906becaf28ec61206efe79b08d2519
Wesley Osborne <wao218@lehigh.edu>
2020-05-06 22:42:56 -0400

    Added PlaceItem and PlaceList and UserPlaces component

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Empire_State_Building%2C_New_York%2C_NY.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: '40.7497031',
+      lng: '-74.0712418'
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Empire_State_Building%2C_New_York%2C_NY.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: '40.7497031',
+      lng: '-74.0712418'
+    },
+    creator: 'u2'
+  }
+];
+
+const UserPlaces = () => {
+  return (
+    <PlaceList items={DUMMY_PLACES} />
+  );
+}
+
+export default UserPlaces;
